c3797fb0d74bd6a5161093831a08adafb329e4a6
Max-IT-ZT <babuskomax@gmail.com>
2023-08-15 23:36:37 +0300

    i-hope-finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,7 +10,7 @@ const renderList = (arr) =>
         `<li class="gallery__item"><a class="gallery__link"href="${item.original}"><img class="gallery__image"src="${item.preview}"data-source="${item.original}"alt="${item.description}"/></a></li>`
     )
     .join("");
-
+let instance = null;
 const listClick = (event) => {
   event.preventDefault();
   if (event.target.tagName !== "IMG") {
@@ -20,13 +20,21 @@ const listClick = (event) => {
   const data = event.target.dataset.source;
   const source = galleryItems.find((item) => item.original === data);
 
-  const instance = basicLightbox.create(`
+  instance = basicLightbox.create(`
     <div class="modal">
         <img class="gallery__image" src="${source.original}" alt="${source.description}" />
     </div>
   `);
 
   instance.show();
+  document.addEventListener("keydown", modalKeydownHandler);
+};
+
+const modalKeydownHandler = (event) => {
+  if (event.key === "Escape") {
+    instance.close();
+    document.removeEventListener("keydown", modalKeydownHandler);
+  }
 };
 
 list.insertAdjacentHTML("beforeend", renderList(galleryItems));
